9cc7fb6866d8082e9d3380598b8e0548c35d2edd
AhmedAzzi <mrahmedazzi@gmail.com>
2026-03-27 21:40:21 +0100

    chore: update base path to root for personal github pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/mcazzi/">
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>
 );
