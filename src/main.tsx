e09334b3f379903d3c8c374dc6d1fa4c3abf2bfb
AZZI MOHAMED CHERIF <mcazzi.prod@gmail.com>
2026-03-27 21:51:41 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/mcazzi">
     <App />
   </BrowserRouter>
 );
